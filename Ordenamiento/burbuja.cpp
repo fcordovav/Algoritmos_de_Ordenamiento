ff7f11e436b03ff426a895d0af5b5c85f45707b9
Felipe Córdova <85969736+PipeCordova@users.noreply.github.com>
2023-08-30 18:15:55 -0400

    actualizando

--- Ordenamiento/burbuja.cpp
+++ Ordenamiento/burbuja.cpp
@@ -32,7 +32,7 @@ void burbuja(int L[], int n) {
     int aux;
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
-            if (L[j] >L[j + 1]) {
+            if (L[j] > L[j + 1]) {
                 aux = L[j];
                 L[j] = L[j + 1];
                 L[j + 1] = aux;
@@ -44,7 +44,7 @@ void burbuja(int L[], int n) {
 void varianteBurbuja(int L[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
-            if (L[j] >L[j + 1]) {
+            if (L[j] > L[j + 1]) {
                 swap(L[j], L[j + 1]);
             }
         }
